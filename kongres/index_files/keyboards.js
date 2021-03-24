cccbd90b46584e4cb5f8e2803fefb8632f364c4c
Benny Lin <bknliem@yahoo.com>
2021-03-24 21:05:22 +0700

    fix bug

--- kongres/index_files/keyboards.js
+++ kongres/index_files/keyboards.js
@@ -2,6 +2,20 @@ var debugKeyboards = [];
 (function() {
   var kmw=KeymanWeb;
   kmw.KRS({
+    KN:"jawa_standar",
+    KI:"Keyboard_jawa_standar",
+    KL:"jawa_standar",
+    KLC:"en",
+    KR:"Europe",
+    KRC:"eu",
+    KFont:{family:"Consolas"},
+    KOskFont:{family:"Arial"},
+    KF:"jawa_standar-1.0.js"
+  });
+  debugKeyboards["jawa_standar"] = {
+    id:"jawa_standar",
+    version:"1.0"
+  };  kmw.KRS({
     KN:"jawa",
     KI:"Keyboard_jawa",
     KL:"jawa",
