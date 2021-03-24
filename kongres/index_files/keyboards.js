97abf76ddda35d30e3f72d834888ca5d832d0344
Benny Lin <bknliem@yahoo.com>
2021-03-24 18:26:45 +0700

    html version

---
+++ kongres/index_files/keyboards.js
@@ -0,0 +1,18 @@
+var debugKeyboards = [];
+(function() {
+  var kmw=KeymanWeb;
+  kmw.KRS({
+    KN:"jawa",
+    KI:"Keyboard_jawa",
+    KL:"jawa",
+    KLC:"en",
+    KR:"Europe",
+    KRC:"eu",
+    KFont:{family:"Consolas"},
+    KOskFont:{family:"Arial"},
+    KF:"jawa-1.0.js"
+  });
+  debugKeyboards["jawa"] = {
+    id:"jawa",
+    version:"1.0"
+  };})();
